d58698a700c95502ec45b673943bbfe4efc96df6
abdulbaseer10 <ggffnubujjh@gmail.com>
2025-05-08 20:41:18 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,134 @@
+
+var express =   30;
+express++; 
+console.log(express); 
+var express1 = 100;
+ --express1;
+console.log(express1);
+var x = 50; 
+var y = x++; 
+console.log(y);
+var y = 50; 
+var z = --y;
+console.log(z);
+num = 10;
+num = 20;
+console.log(num);
+var new1 = 30;
+var new2 = 40;
+alert(new2);
+var calculatedNum = 2 + (2 * 6);
+console.log(calculatedNum);
+var calculatedNum1 = (2 + 2) * 6;
+console.log(calculatedNum1);
+ var calculatedNum2 = (2 + 2) * (4 + 2); 
+console.log(calculatedNum2);
+var calculatedNu3 = ((2 + 2) * 4) + 2;
+console.log(calculatedNu3);
+var count = 2 + 2 * 4 + 10;  
+var cost = (2 + 2) *( 4 + 10);
+console.log(cost);
+var count1 = 2 + 2 * 4 + 10;  
+var cost2 = (2+2 * 4 )+ 10;
+console.log(cost2);
+var count1 =  4 / 2 * 4;  
+var cost2 =   (4 / 2 ) +3  ;
+console.log(cost2);
+var a = 'what is your name?';
+var b = prompt(a);
+console.log(b);
+var age = 'How old are you?';
+var b = prompt(age);
+console.log(b);
+var gender = 'what is your gender?';
+var b = prompt(gender);
+console.log(b);
+var country = 'Which country do you live in?';
+var b = prompt(country);
+console.log(b);
+var city= 'What city do you live in?';
+var b = prompt(city);
+console.log(b);
+var email = 'What is your email address?';
+var b = prompt(email);
+console.log(b);
+var number = 'What is your phone number?';
+var b = prompt(number);
+console.log(b);
+var occupation = 'What is your current occupation?';
+var b = prompt(occupation);
+console.log(b);
+var hobby = 'What is your favorite hobby?';
+var b = prompt(hobby);
+console.log(b);
+var job = 'What is your dream job?';
+var b = prompt(job);
+console.log(b);
+var a = 'what is largest planet in the solar system?';
+var b = 'jupiter';
+var c= prompt(a);
+if (c == b) {
+    alert('Correct! Jupiter is the largest planet in the solar system.');
+}
+else {
+    alert('Incorrect! The correct answer is Jupiter.');
+}
+var capname = 'what is the capital of France?';
+var capans = 'paris';
+var c= prompt(a);
+if (c == capans) {
+    alert('Correct! Paris is the capital of France.');
+}
+else {
+    alert('Incorrect! The correct answer is Paris.');
+}
+var larganimal = 'what is the largest mammal?';
+var animal = 'blue whale';
+var c= prompt(a);
+if (c == animal) {
+    alert('Correct! The blue whale is the largest mammal.');
+}
+else {
+    alert('Incorrect! The correct answer is blue whale.');
+}
+var smallestcountry = 'what is the smallest country in the world?';
+var  country = 'vatican city';
+var c= prompt(a);
+if (c == country) {
+    alert('Correct! Vatican City is the smallest country in the world.');
+}
+else {
+    alert('Incorrect! The correct answer is Vatican City.');
+}
+var largestdesert = 'what is the largest desert in the world?';
+var desert = 'sahara desert';
+var c= prompt(a);
+if (c == desert) {
+    alert('Correct! The Sahara Desert is the largest desert in the world.');
+}
+else {
+    alert('Incorrect! The correct answer is Sahara Desert.');
+}
+      //  chapter8 and 9//
+var num = ("2"+ "2");
+console.log(num);
+message = ("Hello," + "Dolly");
+console.log(message);
+alert("33" + 3); 
+var part1 = "pakistan";
+var part2 = "zindabad";
+alert(part1 + part2);
+var sumtrick = "abdul" + 3;
+console.log(sumtrick);
+var firstName = "abdul";
+var secondName = "baseer";
+var fullName = firstName + secondName;
+console.log(fullName)
+var entername = prompt("Enter your name:");
+var country = prompt("country", "china");
+  var question = "Your species?";
+  var defaultAnswer = "human";
+  var spec = prompt(question, defaultAnswer);
+   var question ="Your species?";
+   var spec = prompt(question, defaultAnswer);
+   
